14cc659a18eadb00e5b52a029cc2aa8dd9616106
elidotco <79024328+elidotco@users.noreply.github.com>
2022-08-21 23:15:53 +0100

    begining

---
+++ data/data.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    image: '../images/im.jpg',
+    des: 'We provide high quality shoes and accessories just for you',
+  },
+
+  {
+    image: '../images/im1.jpg',
+    des: 'We provide high quality shoes and accessories just for you',
+  },
+  {
+    image: '../images/im1.jpg',
+    des: 'We provide high quality shoes and accessories just for you',
+  },
+];
+
+export default data;
